2c8fc6061b710ed8305a420d75051a9c6f701b44
Manohar Joshi <majoshi1@yahoo.com>
2013-07-13 18:10:57 -0400

    Open page in WebView without address bar

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,8 @@
-
 // In-app browser
 document.addEventListener("deviceready", onDeviceReady, false);
 
 // device APIs are available
 //
 function onDeviceReady() {
-    var ref = window.open('https://mobile.trademonster.com/OptionMonster/optionmonster/', '_blank', 'location=yes');
-    //ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-    //ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-    //ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-    //ref.addEventListener('exit', function(event) { alert(event.type); });
+    var ref = window.open('https://mobile.trademonster.com/OptionMonster/optionmonster/', '_self', 'location=no');
 }
