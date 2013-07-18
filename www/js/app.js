c2ac39ca3134b3ddee38352dd212a9c70c925534
Manohar Joshi <majoshi1@yahoo.com>
2013-07-17 20:02:59 -0400

    Try cv

--- www/js/app.js
+++ www/js/app.js
@@ -4,5 +4,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // device APIs are available
 //
 function onDeviceReady() {
-    var ref = window.open('https://mobile.trademonster.com/OptionMonster/optionmonster/', '_self', 'location=no');
+    //var ref = window.open('https://mobile.trademonster.com/OptionMonster/optionmonster/', '_self', 'location=no');
+    var ref = window.open('https://uat.citivelocity.com/fxclickz/', '_self', 'location=no');
 }
